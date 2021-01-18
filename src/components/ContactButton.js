fdc4955474f01d3e120d1cb9021669627eb67276
Peng Yang Guo <pyangguo@gmail.com>
2021-01-18 15:29:29 -0800

    Added contact and resume buttons

---
+++ src/components/ContactButton.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { navigate } from "gatsby"
+import styled from "styled-components"
+
+const Button = styled.button`
+	display: inline-block;
+	font-family: "Montserrat";
+	font-weight: 600;
+	font-size: 0.8rem;
+	letter-spacing: 0.1rem;
+	padding: 1rem;
+	border-radius: 0.5rem;
+	background: transparent;
+	border: 0.1rem solid #34ffdd;
+	color: #34ffdd;
+	max-width: 100%;
+	cursor: pointer;
+	text-align: center;
+
+	&:hover {
+		background-color: #34ffdd14;
+		-webkit-transition: 0.3 ease-in-out;
+		-moz-transition: 0.3 ease-in-out;
+		-ms-transition: 0.3 ease-in-out;
+		-o-transition: 0.3 ease-in-out;
+		transition: 0.3s ease-in-out;
+	}
+`
+
+const ContactButton = ({ path, description }) => {
+	return <Button onClick={() => navigate(`${path}`)}>{description}</Button>
+}
+
+export default ContactButton
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import styled from "styled-components"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import ContactButton from "../components/ContactButton"
 
 const Container = styled.div`
 	margin-left: 20%;
@@ -13,23 +14,53 @@ const Container = styled.div`
 	h4 {
 		color: #34ffdd;
 		font-weight: 100;
+		letter-spacing: 0.1rem;
 		margin-bottom: 1rem;
 	}
 
 	h1 {
 		color: #23d7ff;
+		letter-spacing: 0.1rem;
 		margin-bottom: 1rem;
 	}
 
 	h3 {
 		color: #fff;
 		font-weight: 100;
+		letter-spacing: 0.1rem;
 		margin-bottom: 1rem;
 	}
 
 	p {
 		color: #fff;
+		letter-spacing: 0.1rem;
 		margin-bottom: 2rem;
+	}
+`
+
+const ResumeButton = styled.a`
+	display: inline-block;
+	font-size: 0.8rem;
+	font-weight: 600;
+	letter-spacing: 0.1rem;
+	text-decoration: none;
+	margin-left: 0.8rem;
+	padding: 1rem;
+	border-radius: 0.5rem;
+	background: transparent;
+	border: 0.1rem solid #34ffdd;
+	color: #34ffdd;
+	max-width: 100%;
+	cursor: pointer;
+	text-align: center;
+
+	&:hover {
+		background-color: #34ffdd14;
+		-webkit-transition: 0.3 ease-in-out;
+		-moz-transition: 0.3 ease-in-out;
+		-ms-transition:  0.3 ease-in-out;
+		-o-transition: 0.3 ease-in-out;
+		transition: 0.3s ease-in-out;
 	}
 `
 
@@ -42,6 +73,14 @@ const IndexPage = () => {
 				<h1>Peng Yang Guo.</h1>
 				<h3>I'm a front end developer based in Los Angeles, CA.</h3>
 				<p>Need a developer?</p>
+				<ContactButton path={`/contact`} description={`Contact me`} />
+				<ResumeButton
+					href="/resume.pdf"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Resume
+				</ResumeButton>
 				{/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
 				<Image />
 			</div> */}

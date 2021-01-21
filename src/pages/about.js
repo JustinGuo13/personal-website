4fd7e8ef1e730400e59d36b989bf23b09a4a4d1b
Peng Yang Guo <pyangguo@gmail.com>
2021-01-21 15:31:46 -0800

    Add float animation

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,16 +3,19 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Container } from "../pages/contact"
 
 const AboutPage = () => {
 	return (
 		<Layout>
 			<SEO title="About" />
-			<h1>About</h1>
-			<p>About page information</p>
-			<p>
-				<Link to="/contact">Contact me.</Link>
-			</p>
+			<Container>
+				<h1>About</h1>
+				<p>About page information</p>
+				<p>
+					<Link to="/contact">Contact me.</Link>
+				</p>
+			</Container>
 		</Layout>
 	)
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,28 @@
 import React from "react"
+import styled from "styled-components"
 
 import ContactLinks from "../components/Contact/ContactLinks"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	margin-top: 1rem;
+	color:#fff;
+`
 const ContactPage = () => {
 	return (
 		<Layout>
 			<SEO title="Contact" />
-			<h1>Contact</h1>
-			<p>Contact information</p>
+			<Container>
+				<h1>Contact</h1>
+				<p>Contact information</p>
+			</Container>
+
 			<ContactLinks />
 		</Layout>
 	)
